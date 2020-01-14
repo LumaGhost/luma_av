592706b903f389d1f81fb74b19d5df69da5b4548
LumaGhost <linuxcarson@meep.com>
2020-01-14 18:56:40 -0500

    initial project setup. not functional yet

---
+++ src/include/luma/av/frame.hpp
@@ -0,0 +1,11 @@
+
+
+// https://ffmpeg.org/doxygen/3.3/group__lavu__frame.html
+
+extern "C" {
+#include <libavutil/frame.h>
+}
+
+class frame {
+    
+}
---
+++ test/luma/av/frame_tests.cpp
@@ -0,0 +1,8 @@
+
+#include <luma/av/frame.hpp>
+
+#include <gtest/gtest.h>
+
+TEST(test_test, TestTest) {
+    std::cout << "test" << std::endl;
+}

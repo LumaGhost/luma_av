f564276d1f3285f0442378190661491df879b1df
egendron93 <egendron93@gmail.com>
2020-01-25 18:33:45 +0000

    furthermore, i think ur build should be destroyed

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # luma_av
+## Clangd set up
+- The update alternatives command isnt being run in the dockerfile so 
+you need to tell the vscode-clangd plugin where the clangd-7 executable is.
+The location for the provided docker image is /usr/bin/clangd-7.
+
+I have put the clangd settings in the .vscode settings folder so they should be
+good to go on start up but I would double check just in case.
+## Building with conan
+- ```conan install . -if build --build missing```
+- ```conan source . -sf build```
+- ```conan build . -bf build```
+- I updated the tasks.json with these commands. These are conan commands used to 
+develop locally in the project directory or wherever else and not in the root 
+conan data folder.
---
+++ conanfile.py
@@ -0,0 +1,37 @@
+from conans import ConanFile, CMake, tools
+
+
+class LumaAvConan(ConanFile):
+    name = "luma_av"
+    version = "0.0.0"
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of Lumaav here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
+    generators = "cmake_find_package"
+    exports_sources = "src/*", "test/*", "CMakeLists.txt"
+
+    def requirements(self):
+        self.requires("boost/1.71.0@conan/stable")
+        self.requires("ffmpeg/4.2.1@bincrafters/stable")
+        self.requires("zlib/1.2.11@conan/stable")
+        self.requires("bzip2/1.0.8@conan/stable")
+        self.requires("gtest/1.8.1")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        self.copy("*.hpp", dst="include", src="hello")
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["luma_av"]
+

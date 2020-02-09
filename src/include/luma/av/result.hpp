ba2b357f58f43a399c0c31fd3097bd5bb2ea5d9c
LumaGhost <29874438+LumaGhost@users.noreply.github.com>
2020-02-08 22:08:10 -0500

    move results design notes

--- src/include/luma/av/result.hpp
+++ src/include/luma/av/result.hpp
@@ -8,34 +8,25 @@ extern "C" {
 #include <libavutil/error.h>
 }
 
-
 #include <system_error>
 
 #include <boost/outcome.hpp>
 
-// our own outcome try macro so we can let the user choose between
-//  standalone and boost outcome?
 #define LUMA_AV_OUTCOME_TRY BOOST_OUTCOME_TRY
-
-// https://www.boost.org/doc/libs/develop/libs/outcome/doc/html/motivation/plug_error_code.html
-// https://www.ffmpeg.org/doxygen/2.3/error_8h_source.html
 
 namespace luma {
 namespace av {
 
 namespace outcome = BOOST_OUTCOME_V2_NAMESPACE;
 
-// should this be named errc or error_code?
 enum class errc : int
 {
   success     = 0,
   eof         = AVERROR_EOF,
-  codec_not_found = 1,  // make sure this doesnt clash with any existing ffmpeg codes
-  alloc_failure = AVERROR(ENOMEM) // maybe no_memory is a better name?
+  codec_not_found = 1, 
+  alloc_failure = AVERROR(ENOMEM)
 };
 
-// i *think* this is how beast does it
-//  their own errc enum and an alias to boost error code
 using error_code = std::error_code;
 
 namespace detail
@@ -55,27 +46,9 @@ namespace detail
       }
       char err_buff[buff_size];
       auto ec = av_strerror(errnum, err_buff, buff_size);
-      // todo contract that ec is positive?
-      // or do i really need to use erno here? :(
 
       return std::string(err_buff, buff_size);
     }
-    // OPTIONAL: Allow generic error conditions to be compared to me
-    // virtual std::error_condition default_error_condition(int c) const noexcept override final
-    // {
-    //   switch (static_cast<ConversionErrc>(c))
-    //   {
-    //   case ConversionErrc::EmptyString:
-    //     return make_error_condition(std::errc::invalid_argument);
-    //   case ConversionErrc::IllegalChar:
-    //     return make_error_condition(std::errc::invalid_argument);
-    //   case ConversionErrc::TooLong:
-    //     return make_error_condition(std::errc::result_out_of_range);
-    //   default:
-    //     // I have no mapping for this code
-    //     return std::error_condition(c, *this);
-    //   }
-    // }
   };
 } // detail
 
@@ -86,7 +59,6 @@ inline luma::av::detail::errc_category const& errc_category()
   return c;
 }
 
-
 // Overload the global make_error_code() free function with our
 // custom enum. It will be found via ADL by the compiler if needed.
 inline std::error_code make_error_code(luma::av::errc e)
@@ -94,10 +66,6 @@ inline std::error_code make_error_code(luma::av::errc e)
   return {static_cast<int>(e), errc_category()};
 }
 
-
-// error code: more lightweight but harder to rethrow as an exception
-//  honestly though outcome seems really well design i doubt its much
-//  overhead compared to a regular error code
 template <class T>
 using result = luma::av::outcome::std_result<T, luma::av::error_code>;
 

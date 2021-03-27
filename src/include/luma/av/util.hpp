8b5d95f80155df6df8908a7f9a30e5f1c9c411d5
LumaGhost <29874438+LumaGhost@users.noreply.github.com>
2021-03-27 22:59:41 +0000

    ideas for handling cstrings and pointer paramters

--- src/include/luma/av/util.hpp
+++ src/include/luma/av/util.hpp
@@ -65,7 +65,56 @@ inline [[noreturn]] void terimate() noexcept {
 #endif // LUMA_AV_ENABLE_ASSERTIONS
 
 
+class cstr_view {
+    public:
+    constexpr explicit cstr_view(const char* cstr) : cstr_{cstr} {}
+
+    /*implicit*/ cstr_view(std::string const& str) : cstr_{str.c_str()} {}
+
+    constexpr const char* c_str() const noexcept {
+        return cstr_;
+    }
+
+    private:
+    const char* cstr_;
+};
+namespace detail {
+    template<class T, class U>
+    concept SmartPtr = requires(U ptr) {
+        { ptr.get() } -> std::convertible_to<T>;
+    } && std::is_pointer_v<T>;
+};
+template <class T>
+class AnyPtr {
+    public:
+    using type = std::remove_cv_t<T>;
+    using pointer = type*;
+    using const_pointer = const pointer;
+    template <detail::SmartPtr<pointer> U>
+    /*implicit*/ AnyPtr(U const& u) : ptr_{u.get()} {
+        LUMA_AV_ASSERT(ptr_);
+    }
+    /*implicit*/ AnyPtr(pointer ptr) : ptr_{ptr} {
+        LUMA_AV_ASSERT(ptr_);
+    }
+    const_pointer ptr() const noexcept {
+        return ptr_;
+    }
+    pointer ptr() noexcept {
+        return ptr_;
+    }
+    private:
+    pointer ptr_;
+};
+
 } // luma_av
+
+namespace luma_av_literals {
+
+inline luma_av::cstr_view operator ""_cv (const char* cstr, std::size_t) noexcept {
+    return luma_av::cstr_view{cstr};
+}
+};
 
 
 

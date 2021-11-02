11ee56b89c699eaf816e173c7ec2bacbb4bf9296
LumaGhost <29874438+LumaGhost@users.noreply.github.com>
2021-11-02 03:42:44 +0000

    disable libalsa to fix building with clang13

--- conanfile.py
+++ conanfile.py
@@ -14,6 +14,7 @@ class LumaAvConan(ConanFile):
     default_options = {
         "shared": False,
         "ffmpeg:with_opus": False,
+        "ffmpeg:with_libalsa": False,
         "gtest:build_gmock": False
     }
     generators = "cmake", "cmake_find_package"

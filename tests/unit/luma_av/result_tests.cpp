430adb821720669b8c8a635c5c8ee6d60fa04dae
LumaGhost <29874438+LumaGhost@users.noreply.github.com>
2022-05-11 22:53:14 +0000

    example file formatting (:

--- tests/unit/luma_av/result_tests.cpp
+++ tests/unit/luma_av/result_tests.cpp
@@ -1,24 +1,23 @@
 
-#include <iostream>
-
-#include <luma_av/result.hpp>
-
-
 #include <gtest/gtest.h>
+
+#include <iostream>
+#include <luma_av/result.hpp>
 
 using namespace luma_av;
 
 TEST(result_unit, outcome_example) {
   std::error_code ec = luma_av::errc::success;
 
-  std::cout << "printed by std::error_code as "
-    << ec << " with explanatory message " << ec.message() << std::endl;
+  std::cout << "printed by std::error_code as " << ec
+            << " with explanatory message " << ec.message() << std::endl;
 
-  // We can compare ConversionErrc containing error codes to generic conditions
+  // We can compare ConversionErrc containing error codes to
+  // generic conditions
   std::cout << "ec is equivalent to std::errc::invalid_argument = "
-    << (ec == std::errc::invalid_argument) << std::endl;
+            << (ec == std::errc::invalid_argument) << std::endl;
   std::cout << "ec is equivalent to std::errc::result_out_of_range = "
-    << (ec == std::errc::result_out_of_range) << std::endl;
+            << (ec == std::errc::result_out_of_range) << std::endl;
 
   // can we compare to ints (ffmpeg error codes?)
   ASSERT_EQ(ec.value(), 0);
